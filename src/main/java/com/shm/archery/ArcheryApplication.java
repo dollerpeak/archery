514a96ee133a30713d406ea2af2c568921e1802f
kht <kobold77@empas.com>
2024-02-15 00:04:32 +0900

    초기셋팅

---
+++ src/main/java/com/shm/archery/ArcheryApplication.java
@@ -0,0 +1,13 @@
+package com.shm.archery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcheryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcheryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shm/archery/ArcheryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shm.archery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcheryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
